1dd238228bf12cf5301cfdee5b5493cfb6753dca
Toodie <lucas.vtodt@gmail.com>
2025-09-13 04:57:24 -0300

    feat: add endpoint to save one death record

--- src/death-record/death-record.controller.ts
+++ src/death-record/death-record.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { DeathRecordService } from './death-record.service';
+import { CreateDeathRecordDTO } from './dto/create-death-record.dto';
 
 @Controller('death-record')
 export class DeathRecordController {
   constructor(private readonly deathRecordService: DeathRecordService) {}
+
+  @Post()
+  public async saveDeathRecord(@Body() dto: CreateDeathRecordDTO) {
+    const saved = await this.deathRecordService.save(dto);
+    return saved;
+  }
 }
--- src/death-record/death-record.service.ts
+++ src/death-record/death-record.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { DeathRecord } from './schemas/death-record.schema';
+import { Model } from 'mongoose';
+import { CreateDeathRecordDTO } from './dto/create-death-record.dto';
 
 @Injectable()
-export class DeathRecordService {}
+export class DeathRecordService {
+  constructor(
+    @InjectModel(DeathRecord.name) private DeathRecordModel: Model<DeathRecord>,
+  ) {}
+
+  async save(dto: CreateDeathRecordDTO): Promise<DeathRecord> {
+    const entity = new this.DeathRecordModel(dto);
+    return entity.save();
+  }
+}
